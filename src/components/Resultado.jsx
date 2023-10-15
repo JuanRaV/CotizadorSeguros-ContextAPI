123eaf276cfd9b216028a64bfa16a22f6cf6e704
JuanRaV <juanravirgen@gmail.com>
2023-10-14 20:35:13 -0600

    Implementig useCallback and useRef, and finishing the project

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,3 +1,4 @@
+import { useCallback,useMemo,useRef } from "react"
 import useCotizador from "../hooks/useCotizador"
 import { MARCAS,PLANES } from "../constants"
 
@@ -5,8 +6,14 @@ const Resultado = () => {
     const {resultado,datos} = useCotizador()
     const {marca,plan,año} = datos
 
-    const [nombreMarca] = MARCAS.filter(m=>m.id==Number(marca))
-    const [nombrePlan] = PLANES.filter(p=>p.id==Number(plan))
+    const yearRef = useRef(año)
+
+    const [nombreMarca] = useCallback(
+        MARCAS.filter(m=>m.id==Number(marca))
+    ,[resultado])
+    const [nombrePlan] = useCallback(
+        PLANES.filter(p=>p.id==Number(plan))
+    ,[resultado])
     
 
     if(resultado===0) return null
@@ -22,7 +29,7 @@ const Resultado = () => {
         </p>
 
         <p className="my-2">
-            <span className="font-bold">Año: </span>{año}
+            <span className="font-bold">Año: </span>{yearRef.current}
         </p>
 
         <p className="my-2 text-2xl">
